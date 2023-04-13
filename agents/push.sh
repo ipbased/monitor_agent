ded4fc5c4c91bdb47e1fe8579571ac4c1cbd865e
Vu Tran <baysao@gmail.com>
2023-04-13 16:06:34 +0700

    update

--- agents/push.sh
+++ agents/push.sh
@@ -4,4 +4,14 @@ export URL=http://103.160.88.235
 export CHECK_MK_AGENT=$SITE_ROOT/check_mk_agent.linux
 export TOKEN=$(echo -n ${HOSTNAME} | sha1sum | cut -d' ' -f1)
 export PUSH_URL=push
-python3 $SITE_ROOT/push.py 
+daemon(){
+	while true;do
+		$SITE_ROOT/push.sh	
+		sleep 10
+	done
+}
+if [ $# -eq 0 ];then
+	python3 $SITE_ROOT/push.py 
+else
+	$@
+fi
